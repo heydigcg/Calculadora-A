cc22c8cac3c654eb20f9d1c6f519cbcdaec8885b
heydigcg <31265385+heydigcg@users.noreply.github.com>
2017-08-22 22:53:19 -0500

    Add files via upload

---
+++ Calculadora-6A/Calculadora_6A.Consola/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculadora_6A.Libreria;
+
+namespace Calculadora_6A.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos misMetodos = new Metodos();
+           
+            Console.WriteLine(misMetodos.Saludo());
+           
+            Console.WriteLine(misMetodos.Sumar(11, 10));
+           
+            Console.WriteLine(misMetodos.Restar(25, 5));
+            
+            Console.WriteLine(misMetodos.Multiplicar(5, 10));
+            
+            Console.WriteLine(misMetodos.Dividir(10, 2));
+           
+            Console.ReadKey();
+        }
+    }
+}
